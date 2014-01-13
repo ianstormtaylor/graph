8a297b2c6b941d1f5103446724cdff872a32b7fe
Ian Storm Taylor <ian@ianstormtaylor.com>
2014-01-12 16:18:21 -0800

    swap to component/model with sqlite plugin

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
 
-var each = require('each');
+var db = require('db');
 var fixtures = require('add-fixtures');
-var graphs = require('db-graphs');
+var Graph = require('graph-model');
 var GraphMenu = require('graph-menu');
 var GraphModal = require('graph-modal');
+var series = require('array-series');
+var split = require('split-callback');
 
 /**
- * Add fixtures, then dispatch to home route.
+ * Kick it all off.
  */
 
-fixtures(function (err) {
+series([setup, fixtures, graphs], function (err) {
   if (err) throw err;
-
-  graphs.all(function (err, graphs) {
-    if (err) throw err;
-    var menu = new GraphMenu();
-    each(graphs, menu.add.bind(menu));
-
-    menu.on('select', function (el, graph) {
-      var modal = new GraphModal(graph);
-      modal.show(el);
-    });
-
-    document.body.appendChild(menu.el);
-  });
 });
+
+/**
+ * Setup the database.
+ *
+ * @param {Function} fn
+ */
+
+function setup (fn) {
+  var fns = split(fn);
+  db.transaction(function (t) {
+    var schema = '(id INTEGER PRIMARY KEY AUTOINCREMENT, data)';
+    var sql = 'CREATE TABLE IF NOT EXISTS graph ' + schema;
+    t.executeSql(sql);
+  }, fns.success, fns.error);
+}
+
+/**
+ * Create the graph menu.
+ *
+ * @param {Function} fn
+ */
+
+function graphs (fn) {
+  var menu = new GraphMenu();
+  document.body.appendChild(menu.el);
+
+  menu.on('select', function (el, graph) {
+    var modal = new GraphModal(graph);
+    modal.show(el);
+  });
+
+  Graph.all(function (err, collection) {
+    if (err) return fn(err);
+    collection.each(menu.add.bind(menu));
+    fn();
+  });
+}
